52a6f16c50e9cd740f2a71d910ee094c1cb2d911
Thupten Chakrishar <vajradog@gmail.com>
2015-08-16 11:00:59 -0400

    seed data changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-user1 = User.create(
+User.create(
   email: "admin@damaru.com",
-  display_name: "Default Admin",
+  display_name: "Admin",
   password: "admin",
   role: "admin"
   )
@@ -9,6 +9,9 @@ Post.create(
   title: "Welcome to Damaru",
   body: "Step 1. Head-over to your-domain.com/sign_in | Step 2. Use email: admin@damaru and password: admin to login. Step 3: Change the default email and password. Enjoy!",
   status: "published",
-  user_id: user1.id)
+  user_id: User.first.id)
 
-
+GeneralSetting.create(
+  title: "Damaru",
+  header: "Ruby on Rails CMS",
+  subheader:"default subheader")
