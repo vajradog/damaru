0cfa38f460e0bf2d14220a7f29f700856e4a8189
Thupten Chakrishar <vajradog@gmail.com>
2015-08-16 10:49:05 -0400

    changed db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
-5.times { Fabricate(:post) }
+User.create(
+  email: "admin@damaru.com",
+  display_name: "Default Admin",
+  password: "admin",
+  role: "admin"
+  )
+
+Post.create(
+  title: "Welcome to Damaru",
+  body: "Step 1. Head-over to your-domain.com/sign_in | Step 2. Use email: admin@damaru and password: admin to login. Step 3: Change the default email and password. Enjoy!",
+  status: "published",
+  user_id: user1.id)
+
+
