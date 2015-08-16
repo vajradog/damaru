100ec41cd2435d04c708e507363c53eba4d409ad
Thupten Chakrishar <vajradog@gmail.com>
2015-08-16 11:07:26 -0400

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Post.create(
   title: "Welcome to Damaru",
   body: "Step 1. Head-over to your-domain.com/sign_in | Step 2. Use email: admin@damaru and password: admin to login. Step 3: Change the default email and password. Enjoy!",
   status: "published",
-  user_id: User.first.id)
+  user_id: users.first.id)
 
 GeneralSetting.create(
   title: "Damaru",
